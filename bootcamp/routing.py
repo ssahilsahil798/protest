0ae2c2c52c30334e17dc30383624e44ce5b73365
sahil deswal <ssahilsahil798@gmail.com>
2018-03-09 22:21:02 +0530

    Chat UI ready

--- bootcamp/routing.py
+++ bootcamp/routing.py
@@ -3,8 +3,8 @@ from channels import include
 channel_routing = [
     # Include subrouting from an app with predefined path matching.
     include("bootcamp.activities.routing.websocket_routing",
-            path=r"^/notifications/ws/$"),
-    include("bootcamp.feeds.routing.websocket_routing", path=r"^/feeds/ws/$"),
+            path=r"^/notifications/$"),
+    include("bootcamp.feeds.routing.websocket_routing", path=r"^/feeds/$"),
     include("bootcamp.messenger.routing.websocket_routing",
-            path=r"^/ws")
+            path=r"^/")
 ]
