9cb648308aede308bc212f20839f4e49cec424c3
sahil deswal <ssahilsahil798@gmail.com>
2018-03-24 18:16:30 +0530

    image viewer working

--- bootcamp/feeds/static/js/feeds.js
+++ bootcamp/feeds/static/js/feeds.js
@@ -459,6 +459,7 @@ function displayItems(fileItemList){
     })
 }
 
+    
 
 function uploadFile(fileItem, post_id){
         var policyData;
@@ -552,53 +553,61 @@ function uploadFile(fileItem, post_id){
 
 
 
-function check_pics(){
+  function check_pics(){
 
 
-$(function() {
-  $(".img-w").each(function() {
-    if($(this).parent().is(".img-c")){
-        
-    }else{
-
-
-
-    $(this).wrap("<div class='img-c'></div>")
-    let imgSrc = $(this).find("img").attr("src");
-     $(this).css('background-image', 'url(' + imgSrc + ')');
- }
-  })
-            
-  
-  $(".img-c").click(function() {
-    let w = $(this).outerWidth()
-    let h = $(this).outerHeight()
-    let x = $(this).offset().left
-    let y = $(this).offset().top
-    
-    
-    $(".active").not($(this)).remove()
-    let copy = $(this).clone();
-    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
-    $(".active").css('top', y - 8);
-    $(".active").css('left', x - 8);
-    
-      setTimeout(function() {
-    copy.addClass("positioned")
-  }, 0)
-    
-  }) 
-  
-})
-
-$(document).on("click", ".img-c.active", function() {
-  let copy = $(this)
-  copy.removeClass("positioned active").addClass("postactive")
-  setTimeout(function() {
-    copy.remove();
-  }, 500)
-})
-
+  $('.fs-gal').click(function() {
+    fsGal_DisplayImage($(this));
+  });
+  //Display gallery
+  function fsGal_DisplayImage(obj) {
+    //Clear navigation buttons
+    $('.fs-gal-view > .fs-gal-prev').fadeOut();
+    $('.fs-gal-view > .fs-gal-next').fadeOut();
+    //Set current image
+    var title = obj.attr('alt');
+    if (!title || title == '') { title = obj.attr('title'); }
+    $('.fs-gal-view > h1').text(title);
+    if (!title || title == '') { $('.fs-gal-view > h1').fadeOut(); }
+    else { $('.fs-gal-view > h1').fadeIn(); }
+    var img = obj.data('url');
+    $('.fs-gal-view').css('background-image', 'url('+img+')');
+    //Create buttons
+    var current = $('.fs-gal').index(obj);
+    var prev = current - 1;
+    var next = current + 1;
+    if (prev >= 0) {
+      $('.fs-gal-view > .fs-gal-prev').data('img-index', prev);
+      $('.fs-gal-view > .fs-gal-prev').fadeIn();
+    }
+    if (next < $('.fs-gal').length) {
+      $('.fs-gal-view > .fs-gal-next').data('img-index', next);
+      $('.fs-gal-view > .fs-gal-next').fadeIn();
+    }
+    $('.fs-gal-view').fadeIn(); //Display gallery
+  }
+  //Gallery navigation
+  $('.fs-gal-view .fs-gal-nav').click(function() {
+    var index = $(this).data('img-index');
+    var img = $($('.fs-gal').get(index));
+    fsGal_DisplayImage(img);
+  });
+  //Close gallery
+  $('.fs-gal-view .fs-gal-close').click(function() {
+    $('.fs-gal-view').fadeOut();
+  });
+  //Keyboard navigation
+  $('body').keydown(function(e) {
+    if (e.keyCode == 37) {
+      $('.fs-gal-view .fs-gal-prev').click(); //Left arrow
+    }
+    else if(e.keyCode == 39) { // right
+      $('.fs-gal-view .fs-gal-next').click(); //Right arrow
+    }
+    else if(e.keyCode == 27) { // right
+      $('.fs-gal-view .fs-gal-close').click(); //ESC
+    }
+  });
 }
 
 });
